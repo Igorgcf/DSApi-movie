4315ba4dc191d3c778fe4ecc62826cae7df59cf7
igorgcf <igorTecnologia@outlook.com>
2025-02-25 17:37:57 -0300

    Field Message

---
+++ api-movie/src/main/java/com/devsuperior/movie/resources/exceptions/FieldMessage.java
@@ -0,0 +1,18 @@
+package com.devsuperior.movie.resources.exceptions;
+
+import lombok.Data;
+
+@Data
+public class FieldMessage {
+
+    private String fieldName;
+    private String fieldMessage;
+
+    public FieldMessage(){
+    }
+
+    public FieldMessage(String fieldName, String fieldMessage) {
+        this.fieldName = fieldName;
+        this.fieldMessage = fieldMessage;
+    }
+}

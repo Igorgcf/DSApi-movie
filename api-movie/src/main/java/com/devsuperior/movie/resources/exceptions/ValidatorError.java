6220f8aafb8493bc3a8e2803d43e64883b7a0742
igorgcf <igorTecnologia@outlook.com>
2025-02-25 17:38:07 -0300

    Validator Error

---
+++ api-movie/src/main/java/com/devsuperior/movie/resources/exceptions/ValidatorError.java
@@ -0,0 +1,19 @@
+package com.devsuperior.movie.resources.exceptions;
+
+import lombok.Data;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Data
+public class ValidatorError extends StandardError {
+
+	List<FieldMessage> errors =  new ArrayList<>();
+
+	public void addError(String fieldName, String message){
+
+		errors.add(new FieldMessage(fieldName, message));
+	}
+}
+	
+

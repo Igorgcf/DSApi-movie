5f35499a15ab2777120499e0eb3facad8b59ce6b
igorgcf <igorTecnologia@outlook.com>
2025-02-25 17:36:59 -0300

    E-mail Constraint Impl

---
+++ api-movie/src/main/java/com/devsuperior/movie/validations/EmailConstraintImpl.java
@@ -0,0 +1,25 @@
+package com.devsuperior.movie.validations;
+
+import com.devsuperior.movie.repositories.UserRepository;
+import jakarta.validation.ConstraintValidator;
+import jakarta.validation.ConstraintValidatorContext;
+import org.springframework.beans.factory.annotation.Autowired;
+
+public class EmailConstraintImpl implements ConstraintValidator<EmailConstraint, String> {
+
+    @Autowired
+    private UserRepository repository;
+
+    @Override
+    public void initialize(EmailConstraint constraintAnnotation) {
+        ConstraintValidator.super.initialize(constraintAnnotation);
+    }
+
+    @Override
+    public boolean isValid(String email, ConstraintValidatorContext context) {
+
+        boolean existsByEmail = repository.existsByEmail(email);
+
+        return !existsByEmail;
+    }
+}

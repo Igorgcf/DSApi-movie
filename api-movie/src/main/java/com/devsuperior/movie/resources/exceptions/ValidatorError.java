f3d7b69ca13cd2af2631f3a492ebbb49fb6f0769
igorgcf <igorTecnologia@outlook.com>
2025-02-25 21:00:54 -0300

    Validator Error

--- api-movie/src/main/java/com/devsuperior/movie/resources/exceptions/ValidatorError.java
+++ api-movie/src/main/java/com/devsuperior/movie/resources/exceptions/ValidatorError.java
@@ -4,6 +4,7 @@ import lombok.Data;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 
 @Data
 public class ValidatorError extends StandardError {
@@ -13,6 +14,20 @@ public class ValidatorError extends StandardError {
 	public void addError(String fieldName, String message){
 
 		errors.add(new FieldMessage(fieldName, message));
+	}
+
+	@Override
+	public boolean equals(Object o) {
+		if (this == o) return true;
+		if (o == null || getClass() != o.getClass()) return false;
+		if (!super.equals(o)) return false;
+		ValidatorError that = (ValidatorError) o;
+		return Objects.equals(errors, that.errors);
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(super.hashCode(), errors);
 	}
 }
 	
